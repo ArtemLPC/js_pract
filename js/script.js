b748800152fc14727da1522d4fcb5b8be870c4c6
Artem <timamusic@yandex.ru>
2020-04-10 08:48:41 -0700

    5 commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 'use strict';
 
-let money = prompt('Ваш бюджет на месяц?'),
-    time = prompt('Введите дату в формате YYYY-MM-DD');
+let money, time;
+function start(){
+    while(isNaN(money) || money == '' || money == null){
+        money = +prompt('Ваш бюджет на месяц?');
+    }
+    
+        time = prompt('Введите дату в формате YYYY-MM-DD');
+}
 
+start();
 
 let appData = {
     bujet: money,
@@ -13,21 +20,22 @@ let appData = {
     savings: false
 };
 
-for (let i = 0; i < 2; i++) {
-    let qw1 = prompt('Введите обязательную статью расходов в этом месяце'),
-        qw2 = prompt('Во сколько Вам обойдется ' + qw1 + '?');
-
-        if(typeof(qw1)==='string' && typeof(qw1) != null && typeof(qw2) != null && qw1 != "" && qw2 != "" && qw1.length < 50){
-            console.log('ok');
-
-            appData.expenses[qw1] = qw2;
-        } else {
-            console.log('bed result');
-            i--;
-        }
-
-        
-}
+ function chooseExpenses(){
+    for (let i = 0; i < 2; i++) {
+        let qw1 = prompt('Введите обязательную статью расходов в этом месяце'),
+            qw2 = prompt('Во сколько Вам обойдется ' + qw1 + '?');
+    
+            if(typeof(qw1)==='string' && typeof(qw1) != null && typeof(qw2) != null && qw1 != "" && qw2 != "" && qw1.length < 50){
+                console.log('ok');
+    
+                appData.expenses[qw1] = qw2;
+            } else {
+                console.log('bed result');
+                i--;
+            }
+    }
+ }
+chooseExpenses();
 
 // let i = 0;
 // while (i < 2) {
@@ -46,21 +54,51 @@ for (let i = 0; i < 2; i++) {
     //     i++;
     //  } while (i < 2);
 
-let oneDay = appData.bujet / 30;
+let days = 30;
 
-appData.oneDay = oneDay;
+function detectDayBudget(a,b){
+    let oneDay = (a/b).toFixed();
+    appData.oneDay = oneDay;
+    alert('Ваш дневной лимит равен ' + oneDay);
+}
+detectDayBudget(appData.bujet,days);
 
 console.log(appData);
 
- 
-alert('Ваш дневной лимит равен ' + appData.oneDay);
+// let oneDay = appData.bujet / 30;
+// appData.oneDay = oneDay;
+// alert('Ваш дневной лимит равен ' + appData.oneDay);
 
-if (appData.oneDay < 100){
+
+function detectLevel(a){
+if (a < 100){
     alert('У вас очень маленький уровень достатка');
-} else if (appData.oneDay > 100 && appData.oneDay < 300){
+} else if (a > 100 && a < 300){
     alert('У вас средний уровень достатка');
-} else if (appData.oneDay > 300) {
+} else if (a > 300) {
     alert('У вас высокий уровень достатка');
 } else {
     alert('произошла ошибка');
 }
+}
+
+detectLevel(appData.oneDay);
+
+
+
+function chooseOptExpenses(){
+    for(let i = 1; i < 4; i++){
+        let a = prompt('Статья необязательных расходов?');
+
+        if(typeof(a) === 'string' && typeof(a) != null && a != ''){
+            console.log('done');
+            appData.optionalExpenses[i] = a;
+        }else {
+            console.log('bed');
+            i--;
+        }
+    }
+}
+console.log(appData);
+chooseOptExpenses();
+
